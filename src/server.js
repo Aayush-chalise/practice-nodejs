0721a6007e395643fbd905c1bafccb31c4bd94f4
Aayush Chalise <chaliseaayush2006@gmail.com>
2026-02-15 00:28:53 +0545

    add server file to src

--- src/server.js
+++ src/server.js
@@ -9,7 +9,10 @@ const PORT = 3000;
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+app.get("/about", (req, res) => {
+  res.send("<h1>Hello I am aayush</h2>");
+});
 
 app.listen(PORT, () => {
-  console.log("server is live");
+  console.log("server is live on port :", PORT);
 });
